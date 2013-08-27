391351d21126c60a8f1525a267b884298b2f6d37
Greg Savidge <contactgsavidge@gmail.com>
2013-08-27 14:16:35 -0700

    Added buttonHandler

--- CompareMe/src/com/adigreg/compareme/MainScreen.java
+++ CompareMe/src/com/adigreg/compareme/MainScreen.java
@@ -7,15 +7,20 @@ package com.adigreg.compareme;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainScreen extends Activity
 {
-
+	TextView debugOutput;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_screen);
+		 
+		debugOutput = (TextView) findViewById(R.id.testDebug);
 	}
 
 	@Override
@@ -25,5 +30,17 @@ public class MainScreen extends Activity
 		getMenuInflater().inflate(R.menu.main_screen, menu);
 		return true;
 	}
-
+	
+	
+    
+	public void mainActivityBtnHandler(View v)
+	{
+		switch (v. getId())
+		{
+		   case R.id.getQuestionsBtn:   debugOutput.setText("1"); break;
+		   case R.id.askQuestionsBtn:   debugOutput.setText("2"); break;
+		   case R.id.searchBtn:         debugOutput.setText("3"); break;
+		   case R.id.settingsBtn:       debugOutput.setText("4"); break;
+		}
+	}
 }
