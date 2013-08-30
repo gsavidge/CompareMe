be780a817d0d901f5c1abd3ce3aa18b5c89b108e
Greg Savidge <contactgsavidge@gmail.com>
2013-08-30 08:43:29 -0700

    adi db

--- CompareMe/src/com/adigreg/compareme/MainScreen.java
+++ CompareMe/src/com/adigreg/compareme/MainScreen.java
@@ -5,15 +5,21 @@
 package com.adigreg.compareme;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainScreen extends Activity
 {
+	CompareMeDB myDb;
 	TextView debugOutput;
+	SQLiteDatabase db;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
@@ -22,6 +28,31 @@ public class MainScreen extends Activity
 		setContentView(R.layout.activity_main_screen);
 		 
 		debugOutput = (TextView) findViewById(R.id.testDebug);
+		Log.d("Message", "app is starting");
+		
+		Context con = getApplicationContext();
+		con.deleteDatabase("compareMe.db");
+		
+		myDb = new CompareMeDB(this);
+		Log.d("Hello","Hello");
+		
+		 // lets test to make
+	    myDb.openDatabase();
+	    db=myDb.getReadableDatabase();
+		
+		Cursor c = db.query("users",null,null,null,null,null,null,null);
+	    logCursorInfo(c);
+	    
+	    c = db.query("questions",null,null,null,null,null,null,null);
+	    logCursorInfo(c);
+	    
+	    c = db.query("topics",null,null,null,null,null,null,null);
+	    logCursorInfo(c);
+	    
+	    c = db.query("language_table",null,null,null,null,null,null,null);
+	    logCursorInfo(c);
+		
+		
 	}
 
 	@Override
@@ -68,4 +99,30 @@ public class MainScreen extends Activity
 	{
 		
 	}
+
+	//*************************Database Log Visualization************************/	
+	public void logCursorInfo(Cursor c)
+	{
+	   String TAG="DB Log";
+	   Log.i(TAG,"***Cursor begin***" + " Results: " + c.getCount()+ " Columns: "+c.getColumnCount());       
+	   String header="|";
+	   for (int i=0; i<c.getColumnCount();i++)
+	   {
+	       header+= c.getColumnName(i)+"|";
+	   } 
+	   Log.i (TAG,"Columns"+header);
+	   c.moveToFirst();
+	   while (c.isAfterLast()==false)
+	   {
+	       String results = "|";
+	       for(int i=0;i<c.getColumnCount();i++)
+	       {
+	           results +=c.getString(i)+"|";
+	       }
+	       Log.i(TAG,"Row "+c.getPosition()+ ":"+results);
+	       c.moveToNext();
+	   }
+	   Log.i(TAG,"*** CursorEnd ***");
+    }
 }
+
